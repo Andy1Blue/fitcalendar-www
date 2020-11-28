14fcecb0971d31a7cf84c282b1d9b1ec65e7d885
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-11-28 19:30:13 +0100

    Add DayModal component

--- src/Components/CalendarTiles/CalendarTiles.tsx
+++ src/Components/CalendarTiles/CalendarTiles.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './CalendarTiles.scss';
+import DayModal from '../DayModal/DayModal';
 
 interface CalendarTilesProps {
   className: string;
@@ -11,6 +12,8 @@ interface CalendarTilesProps {
 
 const CalendarTiles = ({ className, month, year, trainings }: CalendarTilesProps) => {
   const daysInMonth = (month: number, year: number) => new Date(year, month, 0).getDate();
+  const [isDayModalVisible, setIsShowDayModal] = useState(false);
+  const [dayModalTraining, setDayModalTraining] = useState(null);
 
   const isToday = (parsedDate: string) => {
     const today = new Date();
@@ -25,15 +28,19 @@ const CalendarTiles = ({ className, month, year, trainings }: CalendarTilesProps
     return parsedDate === `${year}-${month}-${day}`;
   };
 
-  const addTile = (day: number, month: number, year: number) => {
-    const rectElement = document.createElement('rect');
-    rectElement.className = 'day';
+  const openDayModal = (training: any) => (event: any) => {
+    setDayModalTraining(training);
+    setIsShowDayModal(true);
+  };
 
+  const addTile = (day: number, month: number, year: number) => {
     const innerDay = day >= 10 ? day : `0${day}`;
     const innerMonth = month >= 10 ? month : `0${month}`;
-    rectElement.innerHTML = innerDay.toString();
-
     const parsedDate = `${year}-${innerMonth}-${innerDay}`;
+
+    const rectElement = document.createElement('div');
+    rectElement.className = 'day';
+    rectElement.innerHTML = innerDay.toString();
 
     rectElement.setAttribute('date', parsedDate);
     rectElement.setAttribute('description', parsedDate);
@@ -42,12 +49,15 @@ const CalendarTiles = ({ className, month, year, trainings }: CalendarTilesProps
       rectElement.classList.add('today');
     }
 
-    for (const key in trainings) {
-      const training = trainings[key];
+    for (const i in trainings) {
+      const training = trainings[i];
+
       if (training.createdDate.slice(0, 10) === parsedDate) {
+        console.log(training);
+        rectElement.onclick = openDayModal(training);
         rectElement.classList.add('workout');
       }
-  }
+    }
 
     const monthElement = document.getElementById('root').querySelector(`.month${month}`);
     monthElement.appendChild(rectElement);
@@ -69,6 +79,7 @@ const CalendarTiles = ({ className, month, year, trainings }: CalendarTilesProps
   return (
     <div className="calendarTiles">
       <div className={className}></div>
+      {isDayModalVisible && <DayModal isDayModalVisible={(isVisible:boolean) => setIsShowDayModal(isVisible)} training={dayModalTraining} />}
     </div>
   );
 };
--- src/Components/DayModal/DayModal.tsx
+++ src/Components/DayModal/DayModal.tsx
@@ -1,21 +1,38 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import './CalendarTiles.scss';
+import './DayModal.scss';
 
 interface DayModalProps {
+  isDayModalVisible: any;
   training: any;
 }
 
-const DayModal = ({  }: DayModalProps) => {
+const DayModal = ({ isDayModalVisible, training }: DayModalProps) => {
+  const closeDayModal = () => {
+    isDayModalVisible(false);
+  };
 
-
-  useEffect(() => {
-   
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="dayModal">
-      test
+      <div className="dayModal__container">
+        <button type="submit" className="dayModal__close" onClick={closeDayModal}>
+          X
+        </button>
+
+        <div className="dayModal__content">
+          <div className="dayModal__title">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+          <div className="dayModal__stat">{training.createdDate.slice(0, 10)}</div>
+        </div>
+      </div>
     </div>
   );
 };
