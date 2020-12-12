907e01359080fb98df399a28c487279027fac117
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-12-12 14:14:14 +0100

    Header fixes

--- src/http.ts
+++ src/http.ts
@@ -5,7 +5,6 @@ export default axios.create({
   baseURL: API_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-    // 'Content-type': 'application/json',
+    'Content-type': 'application/json',
   },
 });
