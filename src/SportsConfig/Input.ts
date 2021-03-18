5864dd6e2bee94bcf42ea88fde6c71e6d021d400
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2021-03-18 21:45:05 +0100

    Formating

--- src/SportsConfig/Input.ts
+++ src/SportsConfig/Input.ts
@@ -1,6 +1,15 @@
 import { Sport } from '../Types/Training';
 
-export const sportsInput: Sport[] = [Sport.Spinning, Sport.ExerciseBike, Sport.Tabata, Sport.Run, Sport.Walk, Sport.Bike, Sport.Hiking, Sport.Other];
+export const sportsInput: Sport[] = [
+  Sport.Spinning,
+  Sport.ExerciseBike,
+  Sport.Tabata,
+  Sport.Run,
+  Sport.Walk,
+  Sport.Bike,
+  Sport.Hiking,
+  Sport.Other,
+];
 
 export const sportIconMapping = {
   [Sport.Spinning]: '🚴',
